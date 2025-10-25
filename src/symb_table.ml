3fa75883c7594896520dcdf03051abc9a862202f
UmerAhmad211 <umerahmed824@gmail.com>
2025-10-26 01:40:05 +0500

    Pos does not belong in the symb table and remove pop scope

--- src/symb_table.ml
+++ src/symb_table.ml
@@ -1,24 +1,19 @@
 open Ast
 
 module Vars = struct
-  type t = (string, dtype * pos) Hashtbl.t list
+  type t = (string, dtype) Hashtbl.t list
 
   let create () : t = [ Hashtbl.create 4 ]
   let push_scope (env : t) : t = Hashtbl.create 4 :: env
 
-  let pop_scope (env : t) : (t, string) result =
-    match env with
-    | [] -> Error "No scope to pop."
-    | _ :: rem -> Ok rem
-
-  let add (env : t) (name : string) (dt : dtype) (p : pos) =
+  let add (env : t) (name : string) (dt : dtype) =
     match env with
     | [] -> Error "No current scope."
     | scope :: _ ->
         if Hashtbl.mem scope name then
           Error ("Duplicate variable in scope: " ^ name)
         else
-          Ok (Hashtbl.add scope name (dt, p))
+          Ok (Hashtbl.add scope name dt)
 
   let rec find (env : t) (name : string) =
     match env with
@@ -36,18 +31,18 @@ module Vars = struct
 end
 
 module Funcs = struct
-  type t = (string, dtype * pos * (string * dtype) list) Hashtbl.t
+  type t = (string, dtype * (string * dtype) list) Hashtbl.t
 
   let create () : t = Hashtbl.create 4
 
-  let add (env : t) (name : string) (rdt : dtype) (p : pos)
+  let add (env : t) (name : string) (rdt : dtype)
       (params : (string * dtype) list) =
     if Hashtbl.mem env name then
       Error ("Duplicate Function name " ^ name)
     else
-      Ok (Hashtbl.add env name (rdt, p, params))
+      Ok (Hashtbl.add env name (rdt, params))
 
-  let find (env : t) (name : string) : dtype * pos * (string * dtype) list =
+  let find (env : t) (name : string) : dtype * (string * dtype) list =
     Hashtbl.find env name
 
   let mem (env : t) (name : string) : bool = Hashtbl.mem env name
