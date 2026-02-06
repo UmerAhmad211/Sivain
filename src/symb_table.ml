6e168c702ef64773faf82ac07fcaab85019f9630
UmerAhmad211 <umerahmed824@gmail.com>
2026-02-07 01:24:52 +0500

    In symbol table add support to differentiate between global vars and local vars for code gen

--- src/symb_table.ml
+++ src/symb_table.ml
@@ -1,19 +1,23 @@
 open Ast
 
+type var_type =
+  | Gvars
+  | Lvars
+
 module Vars = struct
-  type t = (string, dtype) Hashtbl.t list
+  type t = (string, dtype * var_type) Hashtbl.t list
 
   let create () : t = [ Hashtbl.create 4 ]
   let push_scope (env : t) : t = Hashtbl.create 4 :: env
 
-  let add (env : t) (name : string) (dt : dtype) =
+  let add (env : t) (name : string) (dt : dtype) (vt : var_type) =
     match env with
     | [] -> Error "No current scope."
     | scope :: _ ->
         if Hashtbl.mem scope name then
           Error ("Duplicate variable in scope: " ^ name)
         else
-          Ok (Hashtbl.add scope name dt)
+          Ok (Hashtbl.add scope name (dt, vt))
 
   let rec find (env : t) (name : string) =
     match env with
