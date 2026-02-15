78ed1328b0a06d6bcfd8749ac9fe7b77fc138d4d
UmerAhmad211 <umerahmed824@gmail.com>
2026-02-15 20:06:37 +0500

    Handle cmd args using cmdliner

--- src/svc.ml
+++ src/svc.ml
@@ -1,93 +1,96 @@
 open Util
 open Semant
-open Parsed_ast
 open Cgen
+open Cmdliner
+open Cmdliner.Term.Syntax
 
-let usage_msg =
-  "Usage: svc [--target=<amd64_sysv|arm64|rv64>] <file1> -o <output>"
+type target = Amd64_sysv | Arm64 | Rv64
 
-let input_file = ref ""
+let valid_targets =
+  [ ("amd64_sysv", Amd64_sysv); ("arm64", Arm64); ("rv64", Rv64) ]
 
-(* let build_dir = ".siv-build" *)
-let output_file = ref ""
-let target_arch = ref "amd64_sysv"
-let valid_target = [ "amd64"; "arm64"; "riscv64" ]
+let string_of_target = function
+  | Amd64_sysv -> "amd64_sysv"
+  | Arm64 -> "arm64"
+  | Rv64 -> "rv64"
 
-(* let run_comm comm args = *)
-(*   let pid = Unix.create_process comm args Unix.stdin Unix.stderr Unix.stdout in *)
-(*   let _, status = Unix.waitpid [] pid in *)
-(*   match status with Unix.WEXITED 0 -> () | _ -> failwith (comm ^ "failed.") *)
+let build_dir = ".siv-build"
 
-let anon_fun filename =
-  if !input_file = "" then input_file := filename
+let run_comm comm args =
+  let pid = Unix.create_process comm args Unix.stdin Unix.stderr Unix.stdout in
+  let _, status = Unix.waitpid [] pid in
+  match status with Unix.WEXITED 0 -> () | _ -> failwith (comm ^ "failed.")
+
+let compile ~target ~file ~output =
+  if not (check_filename_exe file) then
+    `Error (true, file ^ ": Sivain files end with .svn.")
   else
-    print_err_usage_fail ~usage:usage_msg
-      "Sivain: Error: Only one file expected."
+    `Ok
+      (let ic = open_in file in
+       let lexbuf = Lexing.from_channel ic in
+       let ast =
+         try Parser.program Lexer.read_token lexbuf with
+         | Lexer.Syntax_Error msg ->
+             prerr_endline msg;
+             exit 1
+         | Parser.Error ->
+             Printf.eprintf "Sivain: Error: Syntax error at %d:%d\n"
+               lexbuf.lex_curr_p.pos_lnum
+               (lexbuf.lex_curr_p.pos_cnum - lexbuf.lex_curr_p.pos_bol);
+             exit 1
+       in
+       close_in ic;
+       check_for_main ast;
+       match check_prog ast with
+       | Ok past ->
+           let fixed_past = List.rev past in
+           let cgened = emitter_driver fixed_past in
+           if not (Sys.file_exists build_dir) then Unix.mkdir build_dir 0o755;
+           let ssa_fname =
+             (build_dir ^ "/"
+             ^ (file |> Filename.basename |> Filename.remove_extension))
+             ^ ".ssa"
+           in
+           let asm_fname = build_dir ^ "/" ^ output ^ ".s" in
+           let oc = open_out ssa_fname in
+           output_string oc cgened;
+           flush oc;
+           close_out oc;
+           run_comm "qbe" [| "qbe"; ssa_fname; "-t"; target; "-o"; asm_fname |];
+           run_comm "cc" [| "cc"; asm_fname; "-o"; output |]
+       | Error (msg, pos) ->
+           Printf.eprintf "Sivain: Error: %s at %d:%d.\n" msg pos.pos_lnum
+             (pos.pos_cnum - pos.pos_bol);
+           exit 1)
 
-let check_target tg =
-  if List.mem tg valid_target then target_arch := tg
-  else
-    print_err_usage_fail ~usage:usage_msg
-      ("Sivain: Error: Invalid target architecture: " ^ tg)
+let target =
+  let doc = "Target architecture: $(docv) (default: System Architecture)." in
+  let targ_enum = Arg.enum valid_targets in
+  Arg.(
+    value & opt targ_enum Amd64_sysv
+    & info [ "t"; "target" ] ~doc ~docv:"TARGET")
 
-let speclist =
-  [
-    ( "--target",
-      Arg.String check_target,
-      "Set target architecture (amd64_sysv, arm64, rv64)." );
-    ("-o", Arg.Set_string output_file, "Set output file name");
-  ]
+let file =
+  let doc = "Input source file." in
+  Arg.(required & pos 0 (some string) None & info [] ~doc ~docv:"FILE")
 
-(* let rec print_scopes = function *)
-(*   | [] -> () *)
-(*   | scope :: rest -> *)
-(*       Hashtbl.iter (fun name _ -> print_endline name) scope; *)
-(*       print_endline "---"; *)
-(*       print_scopes rest *)
+let output =
+  let doc = "Output executable name." in
+  Arg.(required & opt (some string) None & info [ "o" ] ~doc ~docv:"OUTPUT")
 
-let () =
-  Arg.parse speclist anon_fun usage_msg;
-  if !input_file = "" then
-    print_err_usage_fail ~usage:usage_msg "Sivain: Error: Input file expected"
-  else if not (check_filename_exe !input_file) then
-    print_err_usage_fail "Sivain: Error: Invalid file extension.";
-
-  let ic = open_in !input_file in
-  let lexbuf = Lexing.from_channel ic in
-  let ast =
-    try Parser.program Lexer.read_token lexbuf with
-    | Lexer.Syntax_Error msg ->
-        prerr_endline msg;
-        exit 1
-    | Parser.Error ->
-        Printf.eprintf "Sivain: Error: Syntax error at %d:%d\n"
-          lexbuf.lex_curr_p.pos_lnum
-          (lexbuf.lex_curr_p.pos_cnum - lexbuf.lex_curr_p.pos_bol);
-        exit 1
+let svn_cmd =
+  let doc = "Compiler for the Sivain language." in
+  let man =
+    [
+      `S Manpage.s_bugs;
+      `P "Open bug reports in <https://github.com/UmerAhmad211/Sivain/issues>.";
+    ]
   in
-  close_in ic;
-  check_for_main ast;
-  match check_prog ast with
-  | Ok past ->
-      let fixed_past = List.rev past in
-      print_parsed_ast fixed_past;
-      emitter_driver fixed_past
-  | Error (msg, pos) ->
-      Printf.eprintf "Sivain: Error: %s at %d:%d.\n" msg pos.pos_lnum
-        (pos.pos_cnum - pos.pos_bol);
-      exit 1
+  Cmd.make (Cmd.info "svc" ~version:"0.0.1" ~doc ~man)
+  @@ Term.ret
+  @@
+  let+ target = target and+ file = file and+ output = output in
+  compile ~target:(string_of_target target) ~file ~output
 
-(* if not (Sys.file_exists build_dir) then Unix.mkdir build_dir 0o755; *)
-
-(* let ssa_fname = *)
-(*   (build_dir ^ "/" *)
-(*   ^ (!input_file |> Filename.basename |> Filename.remove_extension)) *)
-(*   ^ ".ssa" *)
-(* in *)
-(* let asm_fname = build_dir ^ "/" ^ !output_file ^ ".s" in *)
-(* let oc = open_out ssa_fname in *)
-(* Cgen.emit_program oc ast; *)
-(* flush oc; *)
-(* close_out oc; *)
-(* run_comm "qbe" [| "qbe"; ssa_fname; "-t"; !target_arch; "-o"; asm_fname |]; *)
-(* run_comm "cc" [| "cc"; asm_fname; "-o"; !output_file |] *)
+let main () = Cmd.eval svn_cmd
+let () = if !Sys.interactive then () else exit (main ())
