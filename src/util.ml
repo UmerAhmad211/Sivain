27873a6e358610bb18bfd7f0fc0b61c8dafe53dd
UmerAhmad211 <umerahmed824@gmail.com>
2026-03-06 02:52:39 +0500

    Utils from svc.ml

--- src/util.ml
+++ src/util.ml
@@ -1,5 +1,23 @@
 open Ast
 
+type target = Amd64_sysv | Arm64 | Rv64
+type backends = Qbe | Llvm
+
+let valid_targets =
+  [ ("amd64_sysv", Amd64_sysv); ("arm64", Arm64); ("rv64", Rv64) ]
+
+let valid_backends = [ ("QBE", Qbe); ("LLVM", Llvm) ]
+
+let string_of_target_qbe = function
+  | Amd64_sysv -> "amd64_sysv"
+  | Arm64 -> "arm64"
+  | Rv64 -> "rv64"
+
+let string_of_target_llvm = function
+  | Amd64_sysv -> "--target=x86_64-pc-linux-gnu"
+  | Arm64 -> "--target=aarch64-pc-linux-gnu"
+  | Rv64 -> "--target=riscv64-unknown-linux-gnu"
+
 let check_for_main (prog : Ast.prog) =
   if
     not
